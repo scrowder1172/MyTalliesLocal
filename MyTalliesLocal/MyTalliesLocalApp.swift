f7c72f59d61a9486b6f37c0181b8615a9083aefe
Scott Crowder <scrowder@gmail.com>
2025-03-18 15:11:14 -0500

    Initial Commit

---
+++ MyTalliesLocal/MyTalliesLocalApp.swift
@@ -0,0 +1,20 @@
+//
+// File: MyTalliesLocalApp.swift
+// Project: MyTalliesLocal
+// 
+// Created by SCOTT CROWDER on 3/18/25.
+// 
+// Copyright © Playful Logic Studios, LLC 2025. All rights reserved.
+// 
+
+
+import SwiftUI
+
+@main
+struct MyTalliesLocalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
